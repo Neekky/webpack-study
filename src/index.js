7d33b6d8e0f3c7e3d27643554a6af1b88f339025
cengchao <475383200@qq.com>
2020-04-13 15:43:11 +0800

    aa

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('hello webpack!')
+import isPrime from './util/isPrime';
+import radColor from './util/radColor';
+
+console.log(isPrime(1))
+console.log(isPrime(2))
+console.log(isPrime(3))
+console.log(isPrime(5))
+console.log(isPrime(6))
